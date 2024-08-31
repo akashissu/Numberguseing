3fcb47073471741c2522210bf05cd41f50a65389
Akash Kumar <111304105+akashissu@users.noreply.github.com>
2024-08-31 19:07:10 +0530

    Add files via upload

---
+++ Accordian/Accordion.js
@@ -0,0 +1,21 @@
+import React from 'react';
+// import { RiArrowDropDownLine } from 'react-icons/ri';
+
+
+//  accordionitem component
+const AccordionItem = () => {
+  return(
+    <h1>Accordion</h1>
+  )
+}
+
+// main Accordion component
+const Accordion = () => {
+ return (
+  <div>
+    <AccordionItem />
+  </div>
+ )
+}
+
+export default Accordion;
